1a4c1eacb7002ca89ae94487f775949eb1987b6d
timepiss <kunalkishore.mail@gmail.com>
2015-03-19 20:12:37 +0530

    edited ajax request

--- Google Chrome Extension1/app/js/popup.js
+++ Google Chrome Extension1/app/js/popup.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     $(".ui-autocomplete-input").attr("placeholder", "Enter tags");
     $("#page_title").val(document.title);
     $("#page_url").val(window.location.href);
-    $('#saveSource').on("click", function () {
+    $('#save').on("click", function () {
 
 
         var url = $("#page_url").val();
         var note = $("#page_summary").val();
         //var tagnames = $("#page_tag").val().split(",");
         var title = $("#page_title").val();
-        alert("Sending data url=" + url + "Note=" + note + "tags=" + $("#page_tag").val() + "title=" + title);
+        //alert("Sending data url=" + url + "Note=" + note + "tags=" + $("#page_tag").val() + "title=" + title);
         var tagData = [];
 
         for (var i = 0; i < $(".tagit-label").length; i++) {
