f34fc023dcb00789ad621250f7e835b08662a0b7
timepiss <kunalkishore.mail@gmail.com>
2015-04-16 00:30:14 +0530

    content js

---
+++ Google Chrome Extension1/app/js/content.js
@@ -0,0 +1,7 @@
+﻿chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+      //var gain = request.gen.range.start.split("/");
+      //var x = $("document.all[0]")
+      //if (request.greeting == "dom")
+          sendResponse(document.all[0]);
+  });
